98aa253b6cb91a9bbd68ac15ae32cd7dd81fb3ec
RidiculousBuffal <3322946829@qq.com>
2025-05-19 13:48:18 +0800

    limit content length

--- src/crawler/readability_extractor.py
+++ src/crawler/readability_extractor.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
 # SPDX-License-Identifier: MIT
+import os
 
+from dotenv import load_dotenv
 from readabilipy import simple_json_from_html_string
 
 from .article import Article
+
+load_dotenv()
 
 
 class ReadabilityExtractor:
@@ -11,5 +15,7 @@ class ReadabilityExtractor:
         article = simple_json_from_html_string(html, use_readability=True)
         return Article(
             title=article.get("title"),
-            html_content=article.get("content"),
+            html_content=article.get("content") if os.getenv('MAX_CONTENT_LENGTH') is None else article.get('content')[
+                                                                                                0:os.getenv(
+                                                                                                    'MAX_CONTENT_LENGTH')],
         )
